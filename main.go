f8b913ebf26557e5513195ee768a122f878b4b3c
Adam Alpern <adam.alpern@gmail.com>
2016-03-24 08:31:00 -0700

    Aggregate all passed in catalogs

--- main.go
+++ main.go
@@ -9,35 +9,28 @@ import (
 )
 
 func main() {
-	c, err := luminosity.OpenCatalog(os.Args[1])
+	err := aggregate()
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "%s\n", err)
 		os.Exit(1)
 	}
+}
 
-	lenses, _ := c.GetLenses()
-	cameras, _ := c.GetCameras()
-	distByDate, _ := c.GetPhotoCountsByDate()
-	distByLens, _ := c.GetLensDistribution()
-	distByFocalLength, _ := c.GetFocalLengthDistribution()
-	distByCamera, _ := c.GetCameraDistribution()
-	distByAperture, _ := c.GetApertureDistribution()
-	distByExposureTime, _ := c.GetExposureTimeDistribution()
-
-	data := map[string]interface{}{
-		"cameras": cameras,
-		"lenses":  lenses,
-		"distribution": map[string]interface{}{
-			"by_date":          distByDate,
-			"by_lens":          distByLens,
-			"by_focal_length":  distByFocalLength,
-			"by_camera":        distByCamera,
-			"by_aperture":      distByAperture,
-			"by_exposure_time": distByExposureTime,
-		},
+func aggregate() error {
+	merged := luminosity.NewCatalog()
+	for _, path := range os.Args[1:] {
+		c, err := luminosity.OpenCatalog(path)
+		if err != nil {
+			return err
+		}
+		err = c.Load()
+		if err != nil {
+			return err
+		}
+		merged.Merge(c)
 	}
-
-	print(data)
+	print(merged)
+	return nil
 }
 
 func print(data interface{}) {
